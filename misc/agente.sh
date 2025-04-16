abf9e6445b562211e84898383459a8ac27181dc3
Jesús Palencia <sinfallas@gmail.com>
2025-04-16 09:40:43 -0400

    Update agente.sh

--- misc/agente.sh
+++ misc/agente.sh
@@ -6,12 +6,11 @@ LOCKFILE="/tmp/$(basename $0)_$(whoami)"
 LOCKFD="150"
 token="TOKEN"
 url="https://n8n.example.com/webhook/path"
-if ! [[ -f $HOME/.sid.txt ]]; then
-	echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM > $HOME/.sid.txt
-	sid=$(cat $HOME/.sid.txt)
-else
-	sid=$(cat $HOME/.sid.txt)
-fi
+
+function clean_1() {
+	rm -f /tmp/$(basename $0)_$(whoami)
+	echo -e "\e[00m"
+}
 
 function lock () {
 	echo {LOCKFD}>$LOCKFILE
@@ -23,9 +22,20 @@ function exit_error () {
 	exit 1
 }
 
-function clean_1() {
-	rm -f /tmp/$(basename $0)_$(whoami)
-}
+if ! [[ -f $HOME/.sid.txt ]]; then
+	echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM > $HOME/.sid.txt
+	sid=$(cat $HOME/.sid.txt)
+else
+	sid=$(cat $HOME/.sid.txt)
+fi
+
+if [[ -z $1 ]]; then
+	modo="normal"
+else
+	if [[ "$1" == "retro" ]]; then
+		modo="retro"
+	fi
+fi
 
 trap "clean_1; exit" 0 1 2 3 9 15
 lock || exit_error
@@ -36,10 +46,16 @@ while true; do
 	echo -e "\e[00;1;92mEscribe tu mensaje:\e[00m"
 	read -t 60 prompt
 	if [[ $? -gt 128 ]] ; then
-    		echo -e "\e[00;31mERROR: Tiempo de espera agotado.\e[00m"
-    		exit 1
-    	fi
-	echo ""
-	curl -s --request POST $url -d "chatInput=$prompt&sessionId=$sid" -H "Accept: application/json" -H "Authorization: Bearer $token" | jq -r '.output'
+		echo -e "\e[00;31mERROR: Tiempo de espera agotado.\e[00m"
+		exit 1
+	fi
+	if [[ "$modo" == "normal" ]]; then
+		echo ""
+		curl -s --request POST $url -d "chatInput=$prompt&sessionId=$sid" -H "Accept: application/json" -H "Authorization: Bearer $token" | jq -r '.output'
+	fi
+	if [[ "$modo" == "retro" ]]; then
+		echo -e "\e[00;1;32m"
+		curl -s --request POST $url -d "chatInput=$prompt&sessionId=$sid" -H "Accept: application/json" -H "Authorization: Bearer $token" | jq -r '.output' | pv -qL 10
+	fi
 done
 exit 0

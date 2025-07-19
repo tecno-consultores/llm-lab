415e862349cff3f0311ffc48089c7ca8f6e9a0c6
Jesus Palencia <sinfallas@gmail.com>
2025-07-19 13:29:56 -0400

    * bugfix

--- init/init.sh
+++ init/init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE evolution;
 	GRANT ALL PRIVILEGES ON DATABASE evolution TO "$POSTGRES_USER";
